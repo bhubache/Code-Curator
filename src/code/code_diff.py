bbf6f89759bedc0d0815db04b0ae6f3d7ead1675
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-02-18 21:05:28 -0600

    Preliminary code transformation (#2)

---
+++ src/code/code_diff.py
@@ -0,0 +1,134 @@
+import difflib
+from typing import Union
+
+from .custom_code import CustomCode
+
+class CodeDiff:
+    def __init__(self, source_obj: CustomCode, destination_obj: CustomCode) -> None:
+        self._source_obj = source_obj
+        self._destination_obj = destination_obj
+        self._diff = self._create_diff()
+
+    def __str__(self) -> str:
+        return '\n'.join(self.difference)
+
+    def __iter__(self) -> list:
+        return self.difference.__iter__()
+
+    def __len__(self) -> int:
+        return len(self.difference)
+
+    def __getitem__(self, index: int) -> str:
+        return self.difference[index]
+
+    def __setitem__(self, index: int, item: str) -> None:
+        self.difference[index] = item
+
+    @property
+    def source(self):
+        return self._source_obj
+
+    @property
+    def destination(self):
+        return self._destination_obj
+
+    @property
+    def difference(self):
+        return self._diff
+
+    def line_unique_to_src_code(self, line: str) -> bool:
+        return line.startswith('- ')
+
+    def line_unique_to_dst_code(self, line: str) -> bool:
+        return line.startswith('+ ')
+
+    def line_common_to_both_codes(self, line: str) -> bool:
+        return line.startswith('  ')
+
+    def line_not_present_in_either_code(self, line: str) -> bool:
+        return line.startswith('? ')
+
+    def get_destination_line(self, line_index: int):
+        return self._get_destination_line_attr(line_index=line_index, attr='line')
+
+    def get_destination_line_index(self, line_index: int) -> int:
+        return self._get_destination_line_attr(line_index=line_index, attr='index')
+
+    def _get_destination_line_attr(self, line_index: int, attr: str) -> Union[str, int]:
+        line_exclude_count = 0
+        for i, line in enumerate(self):
+            if self.line_not_present_in_either_code(line) or self.line_unique_to_src_code(line):
+                line_exclude_count += 1
+            elif i == line_index:
+                if attr == 'index':
+                    return i - line_exclude_count
+                elif attr == 'line':
+                    return self.destination[2][i - line_exclude_count]
+                else:
+                    raise RuntimeError(f'Unexpected attr: {attr}')
+        return None
+
+    def get_source_line(self, line_index: int):
+        return self._get_source_line_attr(line_index=line_index, attr='line')
+
+    def get_source_line_index(self, line_index: int) -> int:
+        return self._get_source_line_attr(line_index=line_index, attr='index')
+
+    def _get_source_line_attr(self, line_index: int, attr: str) -> Union[str, int]:
+        line_exclude_count = 0
+        for i, line in enumerate(self):
+            if self.line_not_present_in_either_code(line) or self.line_unique_to_dst_code(line):
+                line_exclude_count += 1
+            elif i == line_index:
+                if attr == 'index':
+                    return i - line_exclude_count
+                elif attr == 'line':
+                    return self.source[2][i - line_exclude_count]
+                else:
+                    raise RuntimeError(f'Unexpected attr: {attr}')
+        return None
+
+    def line_was_added(self, line_index: int) -> bool:
+        if line_index == 0 and self.line_unique_to_dst_code(self[line_index]):
+            return True
+
+        if self.line_unique_to_dst_code(self[line_index]) and not self.line_unique_to_src_code(self[line_index - 1]):
+            return True
+
+        return False
+
+    def _get_diff_char_list(self, word: str) -> list[str]:
+        diff_char_arr = []
+        for i in range(0, len(word), 3):
+            diff_char_arr.append(f'{word[i]}{word[i + 1]}{word[i + 2]}')
+        return diff_char_arr
+
+    def word_contains_added_char(self, word: str) -> bool:
+        diff_chars = self._get_diff_char_list(word)
+
+        for diff_str in diff_chars:
+            if diff_str.startswith('+ '):
+                return True
+        return False
+
+    def word_contains_removed_char(self, word):
+        diff_chars = self._get_diff_char_list(word)
+
+        for diff_str in diff_chars:
+            if diff_str.startswith('- '):
+                return True
+        return False
+
+    def get_sequence_similarity(self, seq_one: str, seq_two: str) -> float:
+        return difflib.SequenceMatcher(None, seq_one, seq_two).ratio()
+
+    def _create_diff(self) -> list[str]:
+        diff = list(difflib.ndiff(self.source.code_string.splitlines(), self.destination.code_string.splitlines()))
+
+        # Remove lines that are not present in either code
+        cleaned_diff = []
+        for line in diff:
+            if self.line_not_present_in_either_code(line): continue
+
+            cleaned_diff.append(line)
+        return cleaned_diff
--- src/leetcode/scenes/code/base_code_scene.py
+++ src/leetcode/scenes/code/base_code_scene.py
@@ -1,6 +1,113 @@
-from manim import *
+from manim import config, Scene, Code, FadeIn, FadeOut, LEFT, UP, Animation, FadeIn, YELLOW, Rectangle
 
-from .base_scene import BaseScene
+from code.custom_code import CustomCode
+from my_animations.code_transform import CodeTransform
 
-class BaseCodeScene(BaseScene):
-    pass
+# from .base_scene import BaseScene
+
+class BaseCodeScene(Scene):
+    config.background_color = '#000E15'
+    def __init__(self, problem_dir: str, aligned_animation_scene) -> None:
+        super().__init__()
+        self._code_src = CustomCode(file_name=r'C:\Users\brand\Documents\ManimCS\src\leetcode\problems\Delete_Node_in_a_Linked_List\required_files\src.java', background_color=config.background_color)
+        self._code_dst = CustomCode(file_name=r'C:\Users\brand\Documents\ManimCS\src\leetcode\problems\Delete_Node_in_a_Linked_List\required_files\dst.java', background_color=config.background_color)
+        self._align_codes(self.code_src, self.code_dst)
+
+    @property
+    def code_src(self):
+        return self._code_src
+
+    @code_src.setter
+    def code_src(self, new_src: CustomCode) -> None:
+        self._code_src = new_src
+
+    @property
+    def code_dst(self):
+        return self._code_dst
+
+    @code_dst.setter
+    def code_dst(self, new_dst: CustomCode) -> None:
+        self._code_dst = new_dst
+
+    def create_highlighter(self):
+        return self.code_src.create_highlighter()
+
+    def code_transform(self):
+        animation = CodeTransform(self.code_src, self.code_dst).animation
+        self.code_dst.highlighter = self.code_src.highlighter
+        self.code_src, self.code_dst = self.code_dst, self.code_src
+        return animation
+
+
+    def _align_codes(self, code_src: CustomCode, code_dst: CustomCode):
+        most_vertical_code, most_width_code = self._get_aligning_codes(self.code_src, self.code_dst)
+
+        self.code_src.align_to(most_vertical_code, UP)
+        self.code_dst.align_to(most_vertical_code, UP)
+
+        self.code_src.align_to(most_width_code, LEFT)
+        self.code_dst.align_to(most_width_code, LEFT)
+
+    def _get_aligning_codes(self, *codes: CustomCode) -> tuple[CustomCode, CustomCode]:
+        most_vertical_code = codes[0]
+        most_width_code = codes[0]
+
+        codes = codes[1:]
+
+        for code in codes:
+            if code.has_more_height(most_vertical_code):
+                most_vertical_code = code
+            if code.has_more_width(most_width_code):
+                most_width_code = code
+
+        return most_vertical_code, most_width_code
+
+
+    def setup(self):
+        pass
+
+    def construct(self):
+        self.play(FadeIn(self.code_src))
+        self.play(FadeIn(self.create_highlighter()))
+
+        self.play(self.code_src.move_highlighter_to_token('Solution', 1))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter_to_token('class', 1))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter_to_token('lass Sol', 1))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter(2))
+        self.wait()
+
+        # self.play(self.code_src.move_highlighter(3))
+        # self.wait()
+
+        self.play(self.code_transform())
+        self.wait()
+
+        self.play(self.code_src.move_highlighter(3))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter_to_token('node', 1))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter_to_token('node.next', 2))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter(-1))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter(1))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter_to_token('node.next', 2))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter_to_token('val', occurrence=1, num_lines=-1))
+        self.wait()
+
+        self.play(self.code_src.move_highlighter_to_token('=', occurrence=1))
+        self.wait()
