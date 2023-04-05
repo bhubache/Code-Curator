15ff7a03afbc7b936383a1e053027ecef53d00bb
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-04-05 15:01:57 -0500

    Compose with Line rather than inherit

--- src/data_structures/edges/singly_directed_edge.py
+++ src/data_structures/edges/singly_directed_edge.py
@@ -5,7 +5,10 @@ from .edge import Edge
 class SinglyDirectedEdge(Edge):
     def __init__(self, start = None, end = None, weight = None, tip_shape = None):
         super().__init__(start=start, end=end, weight=weight)
-        self.add_tip(tip_shape=tip_shape, tip_length=0.2, tip_width=0.2)
+        self._add_tip(tip_shape=tip_shape, tip_length=0.2, tip_width=0.2)
+
+    def _add_tip(self, tip_shape, tip_length, tip_width) -> None:
+        self._line.add_tip(tip_shape=tip_shape, tip_length=tip_length, tip_width=tip_width)
 
     def create_curved_pointer(start: list[float, float, float], end: list[float, float, float], **kwargs):
         singly_directed_edge = SinglyDirectedEdge(0, 1)
@@ -17,5 +20,5 @@ class SinglyDirectedEdge(Edge):
         curved_pointer.set_color('#DBC9B8')
         curved_pointer.set_stroke_width(singly_directed_edge.stroke_width)
 
-        
+
         return curved_pointer
