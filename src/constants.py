d4b444e28227400e1d8acecd8ca66d974d853ea6
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-04-09 12:23:05 -0500

    Add DEFAULT_ELEMENT_FONT_SIZE to constants

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 from colour import Color
 
 DEFAULT_BACKGROUND_COLOR: Color = Color('#000E15')
 DEFAULT_STROKE_WIDTH: int = 2
 DEFAULT_MOBJECT_COLOR: Color = Color('#DBC9B8')
+DEFAULT_ELEMENT_FONT_SIZE: int = 17
