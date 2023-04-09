07d5550f89ad54307462459a95907078f80b4dd9
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-04-08 20:23:20 -0500

    Make 100% coverage of internal logic of Edge class

--- tests/test_non_graphical_units/test_data_structures/test_edges/test_edge.py
+++ tests/test_non_graphical_units/test_data_structures/test_edges/test_edge.py
@@ -2,17 +2,53 @@ import numpy as np
 import pytest
 
 from src.data_structures.edges.edge import Edge
-from src.constants import DEFAULT_MOBJECT_COLOR
+from manim import Line
+from src.constants import DEFAULT_MOBJECT_COLOR, DEFAULT_STROKE_WIDTH
 
 @pytest.fixture
 def default_edge() -> Edge:
     return Edge()
 
-def test_default_edge_start(default_edge: Edge) -> None:
+def test_default_start(default_edge: Edge) -> None:
     assert np.array_equal(default_edge.start, np.array([-1, 0, 0]))
 
-def test_default_edge_end(default_edge: Edge) -> None:
+def test_default_end(default_edge: Edge) -> None:
     assert np.array_equal(default_edge.end, np.array([1, 0, 0]))
 
-def test_default_edge_color(default_edge: Edge) -> None:
-    assert default_edge.color == DEFAULT_MOBJECT_COLOR
+def test_default_weight(default_edge: Edge) -> None:
+    assert default_edge.weight is None
+
+def test_default_vertical_length(default_edge: Edge) -> None:
+    assert default_edge.vertical_length == 0
+
+def test_default_horizontal_length(default_edge: Edge) -> None:
+    assert default_edge.horizontal_length == 2
+
+def test_default_length(default_edge: Edge) -> None:
+    assert default_edge.length == 2
+
+def test_default_line(default_edge: Edge) -> None:
+    assert type(default_edge.line) == Line
+
+
+@pytest.fixture
+def custom_edge() -> Edge:
+    return Edge(start=[-1, -2, 0], end=[2, 2, 0], line_color='#FFFFFF', line_stroke_width=5, weight=16)
+
+def test_custom_start(custom_edge: Edge) -> None:
+    assert np.array_equal(custom_edge.start, np.array([-1, -2, 0]))
+
+def test_custom_end(custom_edge: Edge) -> None:
+    assert np.array_equal(custom_edge.end, np.array([2, 2, 0]))
+
+def test_custom_weight(custom_edge: Edge) -> None:
+    assert custom_edge.weight == 16
+
+def test_custom_vertical_length(custom_edge: Edge) -> None:
+    assert custom_edge.vertical_length == 4
+
+def test_custom_horizontal_length(custom_edge: Edge) -> None:
+    assert custom_edge.horizontal_length == 3
+
+def test_custom_length(custom_edge: Edge) -> None:
+    assert custom_edge.length == 5
