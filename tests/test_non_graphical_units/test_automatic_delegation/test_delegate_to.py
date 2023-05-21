d478cfcc258ad2c0f0fc857e774832e06c0ad4d5
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-05-20 23:17:40 -0500

    Add delegate to tests

--- tests/test_non_graphical_units/test_automatic_delegation/test_delegate_to.py
+++ tests/test_non_graphical_units/test_automatic_delegation/test_delegate_to.py
@@ -18,16 +18,8 @@ from src.custom_vmobject import CustomVMobject
 )
 class ManimPropertyMobject(CustomVMobject):
     def __init__(self) -> None:
+        super().__init__()
         self._line = Line(color=Color('#FFFFFF'))
-        # self._non_manim_property = 0
-
-    # @property
-    # def color(self) -> Color:
-    #     return self._line.color
-
-    # @color.setter
-    # def color(self, new_color: str | Color) -> None:
-    #     self._line.color = new_color
 
 
 @pytest.fixture
@@ -40,24 +32,68 @@ def test_invalid_manim_property_delegate(manim_delegate: ManimPropertyMobject) -
         manim_delegate.invalid_manim_property_delegate
 
 
+def test_valid_manim_property_delegate(manim_delegate: ManimPropertyMobject) -> None:
+    assert manim_delegate.color == Color('#FFFFFF')
+
+
+def test_valid_manim_property_delegate_assignment(manim_delegate: ManimPropertyMobject) -> None:
+    manim_delegate.color = Color('#000000')
+    assert manim_delegate.color == Color('#000000')
+
+
+# TODO: Determine if manim properties can or cannot be deleted
+# def test_valid_manim_property_delegate_deletion(manim_delegate: ManimPropertyMobject) -> None:
+#     del manim_delegate.color
+#     manim_delegate.color
+
+
 def test_delegate_that_is_not_passed_to_delegate_to(manim_delegate: ManimPropertyMobject) -> None:
-    pass
+    with pytest.raises(AttributeError):
+        manim_delegate.property_not_passed_in_to_delegate_to
 
 
-# def test_delegate_missing_manim_property() -> None:
-#     with pytest.raises(Exception):
-#         manim_property_mobject = ManimPropertyMobject()
+class MobjectWithNonManimProperty(CustomVMobject):
+    def __init__(self) -> None:
+        super().__init__()
+        self._non_manim_property: int = 5
+
+    @property
+    def non_manim_property(self) -> int:
+        return self._non_manim_property
+
+    @non_manim_property.setter
+    def non_manim_property(self, new_value: int) -> None:
+        self._non_manim_property = new_value
+
+    @non_manim_property.deleter
+    def non_manim_property(self) -> None:
+        del self._non_manim_property
 
 
-# def test_valid_delegate() -> None:
-#     manim_property_mobject = ManimPropertyMobject()
-#     assert manim_property_mobject.stroke_width == 100
+@delegate_to(
+    MobjectWithNonManimProperty,
+    to='_mobj',
+    normal_include={
+        '_non_manim_property',
+    },
+)
+class DelegatingMobjectWithNonManimProperty(CustomVMobject):
+    def __init__(self) -> None:
+        super().__init__()
+        self._mobj = MobjectWithNonManimProperty()
 
-def test_color_delegate() -> None:
-    manim_property_mobject = ManimPropertyMobject()
-    assert manim_property_mobject.color == Color('#FFFFFF')
+
+def test_mobj_with_non_manim_delegated_property_retrieval():
+    obj = MobjectWithNonManimProperty()
+    assert obj.non_manim_property == 5
 
 
-def test_another_delegate() -> None:
-    manim_property_mobject = ManimPropertyMobject()
-    assert manim_property_mobject.color == Color('#FFFFFF')
+def test_mobj_with_non_manim_delegated_property_assignment():
+    obj = MobjectWithNonManimProperty()
+    obj.non_manim_property = 100
+    assert obj.non_manim_property == 100
+
+
+def test_mobj_with_non_manim_delegated_property_deletion() -> None:
+    obj = MobjectWithNonManimProperty()
+    del obj.non_manim_property
