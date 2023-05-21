5aac82dfb807309f6fbc75026f97a80ba1b4472d
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-05-21 14:34:48 -0500

    Add another test

--- tests/test_graphical_units/test_data_structures/test_edges/test_edge.py
+++ tests/test_graphical_units/test_data_structures/test_edges/test_edge.py
@@ -12,3 +12,9 @@ __module_test__ = 'edges'
 def test_edge(scene: Scene) -> None:
     edge: Edge = Edge()
     scene.play(Animation(edge))
+
+
+@frames_comparison(last_frame=False)
+def test_edge_longer(scene: Scene) -> None:
+    edge = Edge(start=[0, 0, 0], end=[2, 2, 0])
+    scene.play(Animation(edge))
