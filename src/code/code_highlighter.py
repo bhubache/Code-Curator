45ac6d2e48d1939459a71b1627cb68bba4b2f525
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-05-30 16:49:02 -0500

    Fix code_highlighter height

--- src/code/code_highlighter.py
+++ src/code/code_highlighter.py
@@ -1,11 +1,18 @@
-import re
+from __future__ import annotations
 
-from manim import Rectangle, YELLOW, Code, LEFT, UP, DOWN, AnimationGroup, Succession
+from manim import AnimationGroup
+from manim import Code
+from manim import DOWN
+from manim import LEFT
+from manim import Rectangle
+from manim import YELLOW
+
 
 class CodeHighlighter(Rectangle):
-    def __init__(self, code: Code, height: float = None, width: float = None, color = YELLOW, stroke_width = 0):
-        self._custom_init_super(code=code, height=height, width=width, color=color, stroke_width=stroke_width)
-
+    def __init__(self, code: Code, height: float = None, width: float = None, color=YELLOW, stroke_width=0):
+        self._custom_init_super(
+            code=code, height=height, width=width, color=color, stroke_width=stroke_width,
+        )
 
     @property
     def code(self) -> Code:
@@ -34,21 +41,35 @@ class CodeHighlighter(Rectangle):
         self._code = code
         self._curr_line_num = 0
         self.set_opacity(0.5)
-        self.align_to(self.code[2][0], LEFT + UP)
+        self.align_to(self.code[2][self._curr_line_num], DOWN)
+        self.align_to(self.code[2][self._curr_line_num], LEFT)
 
     def move_to_token(self, token: str, occurrence: int = 1):
         '''
         For the line the highlighter is on, you can specify a token for the
         highlighter to cover
         '''
-        code_token_obj = self.code.get_token_at_line(token=token, line_num=self.curr_line_num, occurrence=occurrence)
-        return AnimationGroup(AnimationGroup(self.animate.become(CodeHighlighter(code=self.code, width=code_token_obj.width)).align_to(code_token_obj, LEFT + DOWN)))
-
+        code_token_obj = self.code.get_token_at_line(
+            token=token, line_num=self.curr_line_num, occurrence=occurrence,
+        )
+        return AnimationGroup(
+            AnimationGroup(
+                self.animate.become(
+                    CodeHighlighter(
+                        code=self.code,
+                        width=code_token_obj.width,
+                    ).align_to(self.code.get_line_at(self.curr_line_num), DOWN)
+                    .align_to(code_token_obj, LEFT),
+                ),
+            ),
+        )
 
     def move(self, num_lines: int) -> None:
         new_line_num = self.curr_line_num + num_lines
         if new_line_num < 0 or new_line_num >= self.code.num_lines:
-            raise IndexError(f'{new_line_num} is out of bounds for {self.code.num_lines}')
+            raise IndexError(
+                f'{new_line_num} is out of bounds for {self.code.num_lines}',
+            )
 
         self.curr_line_num = new_line_num
 
