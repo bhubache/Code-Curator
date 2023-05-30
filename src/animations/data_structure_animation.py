0dde775f28c3e376e3aa7eb2aedb9567f3f6faff
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-05-30 16:40:54 -0500

    Fix adding subanimations after DataStructureAnimation creation

--- src/animations/data_structure_animation.py
+++ src/animations/data_structure_animation.py
@@ -1,22 +1,29 @@
 from __future__ import annotations
 
-from animations.singly_linked_list import data_structure_animator as ds_animator
+from typing import TYPE_CHECKING
+
+from animations.singly_linked_list.subanimations.base_subanimation import BaseSubanimation
 from custom_logging.custom_logger import CustomLogger
-from data_structures import singly_linked_list as sll_m
 from manim import Animation
+
+if TYPE_CHECKING:
+    from data_structures.singly_linked_list import SinglyLinkedList
+    from animations.singly_linked_list.data_structure_animator import DataStructureAnimator
 
 from .subanimation_group import SubanimationGroup
 logger = CustomLogger.getLogger(__name__)
 
 
 class DataStructureAnimation(Animation):
-    def __init__(self, sll, data_structure_animator) -> None:
+    def __init__(self, sll: SinglyLinkedList, data_structure_animator: DataStructureAnimator) -> None:
         super().__init__(sll, run_time=data_structure_animator.get_run_time())
-        self._sll: sll_m.SinglyLinkedList = sll
-        self._animator: ds_animator.DataStructureAnimator = data_structure_animator
+        self._sll: SinglyLinkedList = sll
+        self._animator: DataStructureAnimator = data_structure_animator
         self._subanimation_group: SubanimationGroup = self._animator.get_subanimation_group()
 
     def begin(self) -> None:
+        self.run_time = self._animator.get_run_time()
+        self._subanimation_group = self._animator.get_subanimation_group()
         self._subanimation_group.init_run_time()
         super().begin()
 
@@ -27,3 +34,9 @@ class DataStructureAnimation(Animation):
         self._subanimation_group.clean_up_from_scene(scene)
         self._animator.clean_up_mobject()
         return super().clean_up_from_scene(scene)
+
+    def insert_subanimation(self, index: int, subanimation: BaseSubanimation) -> None:
+        self._subanimation_group.insert(index, subanimation)
+
+    def remove_subanimation(self, index: int) -> None:
+        self._subanimation_group.remove(self._subanimation_group.get(index))
