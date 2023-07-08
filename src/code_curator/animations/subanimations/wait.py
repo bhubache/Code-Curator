2f4f8e7c1490388fabc6824d7895248d188b7aa6
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-07-07 21:26:59 -0500

    Prepare for trip

---
+++ src/code_curator/animations/subanimations/wait.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from code_curator.custom_logging.custom_logger import CustomLogger
+from code_curator.animations.singly_linked_list.subanimations.leaf_subanimation import LeafSubanimation
+
+logger = CustomLogger.getLogger(__name__)
+
+
+class WaitSubanimation(LeafSubanimation):
+    def __init__(self, run_time: int = 1):
+        super().__init__(None, run_time=run_time)
+
+    def begin(self) -> None:
+        pass
+
+    def interpolate(self, alpha: float) -> None:
+        pass
+
+    def clean_up_from_animation(self) -> None:
+        pass
+
+    def create_successive_counterpart(self) -> LeafSubanimation:
+        return Wait()
