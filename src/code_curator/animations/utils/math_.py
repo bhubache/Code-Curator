b45fbcf1ae784aa8c9d28f4a2076f7926336754f
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-08-27 22:24:16 -0500

    Add func to map onevalue from one range to another

--- src/code_curator/animations/utils/math_.py
+++ src/code_curator/animations/utils/math_.py
@@ -6,3 +6,14 @@ import math
 def degrees_to_radians(angle_in_degrees: float) -> float:
     """Convert ``angle_in_degrees`` to radians."""
     return (angle_in_degrees * math.pi) / 180
+
+
+def value_from_range_to_range(
+    value: float,
+    init_min: float,
+    init_max: float,
+    new_min: float,
+    new_max: float,
+) -> float:
+    # TODO: Comment an explanation for this
+    return new_min + ((new_max - new_min) / (init_max - init_min)) * (value - init_min)
