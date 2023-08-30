5e39a3ac7f86c5f0ede38275d9fceff2ad6f5532
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-08-29 23:02:55 -0500

    Flesh out more of first video animations

--- src/code_curator/leetcode/problems/Delete_Node_in_a_Linked_List/scenes/problem_analysis.py
+++ src/code_curator/leetcode/problems/Delete_Node_in_a_Linked_List/scenes/problem_analysis.py
@@ -6,9 +6,15 @@ from manim import Animation
 from manim import AnimationGroup
 from manim import Circle
 from manim import FadeIn
+from manim import Flash
+from manim import GREEN
+from manim import Circumscribe
 from manim import Line
 from manim import Square
 from manim import UP
+from manim import LEFT
+from manim import Tex
+from manim import TransformMatchingShapes
 from manim import Wait
 from manim import Write
 
@@ -16,6 +22,7 @@ from .present_problem import CONSTRAINTS
 from code_curator.animations.animation_generator import AnimationGenerator
 from code_curator.animations.subanimations.wait import WaitSubanimation
 from code_curator.animations.utils.utils import overriding_animation
+from code_curator.animations.utils.utils import transition_overriding_start
 from code_curator.data_structures.pointers.simple_pointer import SimplePointer
 from code_curator.data_structures.singly_linked_list import SinglyLinkedList
 from code_curator.leetcode.problem_text import ProblemText
@@ -45,7 +52,7 @@ class ProblemAnalysis(BaseProblemAnalysis):
         self,
         problem_dir: str,
         aligned_animation_scene: CompositeAnimationScript,
-    ):
+    ) -> None:
         super().__init__(
             constraints=CONSTRAINTS,
             explanations=EXPLANATIONS,
@@ -53,23 +60,69 @@ class ProblemAnalysis(BaseProblemAnalysis):
             aligned_animation_scene=aligned_animation_scene,
         )
 
+    class ParallelAnimation:
+        def parallel_1(self):
+            yield FadeIn(Circle(0.1))
+
+        def parallel_2(self):
+            yield FadeIn(Circle(0.5))
+
+        def parallel_3(self):
+            yield FadeIn(Circle(1))
+
     @overriding_animation
     class explain_constraint_one(AnimationGenerator):
-        class inner_something(AnimationGenerator):
-            def inner_one(self):
-                yield FadeIn(Square(0.1))
 
-            def inner_two(self):
-                yield FadeIn(Circle(0.1))
+        def initialize(self) -> None:
+            self.fourth_constraint = self._get_constraint_mobject(4).copy()
+            self.first_check = 'is in the list'
+            self.second_check = 'is not a tail node'
+            self.check_list = ProblemText.create_constraints_list([self.first_check, self.second_check])
+            self.sll = SinglyLinkedList(0)
 
-        def one(self):
-            yield FadeIn(Square())
+        @transition_overriding_start
+        def highlight_fourth_constraint(self):
+            self.check_list.to_edge(UP).to_edge(LEFT)
+            yield TransformMatchingShapes(self.fourth_constraint, self.check_list)
 
-        def two(self):
-            yield FadeIn(Circle())
+        def at_least_one_node(self):
+            in_the_list_check_mark = Tex(r'$\checkmark$').scale(0.6).set_color(GREEN)
+            in_the_list_check_mark.next_to(self.check_list.get_part_by_tex(self.second_check))
+            in_the_list_check_mark.align_to(self.check_list, UP)
+            yield AnimationGroup(
+                FadeIn(in_the_list_check_mark),
+                FadeIn(self.sll),
+            )
 
-        def aa(self):
-            yield FadeIn(Line())
+        def bars_node(self):
+            yield Circumscribe(self.check_list.get_part_by_tex(self.second_check), run_time_proportion=0.4)
+
+        def both_the_head(self):
+            yield Flash(self.sll.head_pointer, run_time=0.1)
+
+        def and_tail(self):
+            yield Flash(self.sll.tail_pointer, run_time=0.1)
+
+        def at_least_two_nodes(self):
+            is_not_a_tail_node_checkmark = Tex(r'$\checkmark$').scale(0.6).set_color(GREEN)
+            is_not_a_tail_node_checkmark.next_to(self.check_list.get_part_by_tex(self.second_check))
+            # is_not_a_tail_node_checkmark.align_to(self.check_list, UP)
+            yield AnimationGroup(
+                self.sll.add_last(data=1)
+                .subsequently_fade_in_container()
+                .with_fade_in_pointer()
+                .with_center_sll()
+                .with_move_tail()
+                .build_animation(),
+                FadeIn(is_not_a_tail_node_checkmark),
+            )
+
+    def present_second_constraint(self):
+        yield FadeIn(Circle())
+
+    @overriding_animation
+    def explain_constraint_two(self):
+        yield FadeIn(Circle())
 
     def explanation_1(self) -> Iterable[Animation]:
         sll = SinglyLinkedList(0)
