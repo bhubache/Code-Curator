40638ede842283911bb5ca084d3539c07655c4a6
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-11-15 10:27:38 -0600

    Encapsulate Edge

--- src/code_curator/data_structures/graph.py
+++ src/code_curator/data_structures/graph.py
@@ -9,12 +9,18 @@ from manim import Circle
 from manim import Line
 from manim import Mobject
 
-from code_curator.constants import DEFAULT_ELEMENT_FONT_SIZE
 from code_curator.custom_vmobject import CustomVMobject
 from code_curator.data_structures.element import Element
 
 if TYPE_CHECKING:
     from colour import Color
+
+
+DEFAULT_COLOR = BLACK
+DEFAULT_LABEL_FONT_SIZE = 15
+DEFAULT_CONTENTS_FONT_SIZE = 15
+DEFAULT_VERTEX_RADIUS = 0.2
+DEFAULT_VERTEX_STROKE_WIDTH = 0.75
 
 
 class Vertex(CustomVMobject):
@@ -41,10 +47,18 @@ class Vertex(CustomVMobject):
             Vertex._default_label += 1
 
         if not isinstance(label, Mobject):
-            label = Element(label, color=BLACK, font_size=DEFAULT_ELEMENT_FONT_SIZE - 2)
+            label = Element(
+                label,
+                color=DEFAULT_COLOR,
+                font_size=DEFAULT_LABEL_FONT_SIZE,
+            )
 
         if container is None:
-            container = Circle(color=BLACK, radius=0.2, stroke_width=0.75)
+            container = Circle(
+                color=DEFAULT_COLOR,
+                radius=DEFAULT_VERTEX_RADIUS,
+                stroke_width=DEFAULT_VERTEX_STROKE_WIDTH,
+            )
 
         self.label = label
         self.container = container
@@ -56,15 +70,17 @@ class Vertex(CustomVMobject):
         if self.contents is not None:
             self.contents = Element(
                 contents,
-                color=BLACK,
-                font_size=DEFAULT_ELEMENT_FONT_SIZE - 2,
+                color=DEFAULT_COLOR,
+                font_size=DEFAULT_CONTENTS_FONT_SIZE,
             )
             self.container.add(self.contents)
 
         self.container.move_to(position)
 
         if label_out:
-            circumscribing_circle = Circle(stroke_width=0.75).surround(
+            circumscribing_circle = Circle(
+                stroke_width=container.stroke_width,
+            ).surround(
                 container,
                 buffer_factor=1 + label_dist,
             )
@@ -80,6 +96,67 @@ class Vertex(CustomVMobject):
 
     def label_is(self, value: str) -> bool:
         return self.label.value == value
+
+
+class Edge(CustomVMobject):
+    def __init__(
+        self,
+        vertex_one: Vertex,
+        vertex_two: Vertex,
+        *,
+        label: str | None = None,
+        color: str | Color = BLACK,
+        opacity: float = 1.0,
+        line_stroke_width: float = 0.75,
+        label_distance_proportion: float = 0.5,
+        label_line_sep: float = 0.1,
+        label_container: Mobject | None = None,
+        label_out: bool = False,
+        label_revolve_angle_in_degrees: float = 0.0,
+        label_rotate_angle_in_degrees: float = 0.0,
+        tip_length: float = 0.1,
+        tip_width: float = 0.075,
+        directedness: str = "-",
+    ) -> None:
+        super().__init__()
+        self.vertex_one = vertex_one
+        self.vertex_two = vertex_two
+        self.line = Line(
+            vertex_one,
+            vertex_two,
+            color=DEFAULT_COLOR,
+            stroke_width=line_stroke_width,
+        )
+
+        if directedness.endswith(">"):
+            self.line.add_tip(tip_length=tip_length, tip_width=tip_width)
+
+        if directedness.startswith("<"):
+            self.line.add_tip(tip_length=tip_length, tip_width=tip_width, at_start=True)
+
+        def shortest_path_updater(edge_to_update: Line) -> None:
+            reference_line = Line(
+                vertex_one.get_center(),
+                vertex_two.get_center(),
+            )
+
+            edge_to_update.become(
+                Line(
+                    reference_line.point_from_proportion(
+                        vertex_one.container.radius / reference_line.get_length(),
+                    ),
+                    reference_line.point_from_proportion(
+                        1 - (vertex_two.container.radius / reference_line.get_length()),
+                    ),
+                )
+                .add_tip(edge_to_update.tip)
+                .match_style(edge_to_update),
+            )
+
+        self.line.add_updater(shortest_path_updater)
+        self.line.add_updater(shortest_path_updater)
+
+        self.add(self.line)
 
 
 class Graph(CustomVMobject):
@@ -105,18 +182,7 @@ class Graph(CustomVMobject):
         self,
         vertex_one: str | Mobject,
         vertex_two: str | Mobject,
-        *,
-        label: str | None = None,
-        color: str | Color = BLACK,
-        opacity: float = 1.0,
-        line_stroke_width: float = 1.0,
-        label_distance_proportion: float = 0.5,
-        label_line_sep: float = 0.1,
-        label_container: Mobject | None = None,
-        label_out: bool = False,
-        label_revolve_angle_in_degrees: float = 0.0,
-        label_rotate_angle_in_degrees: float = 0.0,
-        directedness: str = "-",
+        **kwargs,
     ) -> None:
         if isinstance(vertex_one, str):
             vertex_one = self.get_vertex(vertex_one)
@@ -124,43 +190,17 @@ class Graph(CustomVMobject):
         if isinstance(vertex_two, str):
             vertex_two = self.get_vertex(vertex_two)
 
-        edge = Line(vertex_one, vertex_two, color=BLACK, stroke_width=0.75)
-
-        if directedness.endswith(">"):
-            edge.add_tip(tip_length=0.1, tip_width=0.075)
-
-        if directedness.startswith("<"):
-            edge.add_tip(tip_length=0.1, tip_width=0.075, at_start=True)
-
-        def shortest_path_updater(edge_to_update: Line) -> None:
-            reference_line = Line(
-                vertex_one.container.get_center(),
-                vertex_two.container.get_center(),
-                color=BLACK,
-            )
-
-            edge_to_update.become(
-                Line(
-                    reference_line.point_from_proportion(
-                        vertex_one.container.radius / reference_line.get_length(),
-                    ),
-                    reference_line.point_from_proportion(
-                        1 - (vertex_two.container.radius / reference_line.get_length()),
-                    ),
-                )
-                .add_tip(edge_to_update.tip)
-                .match_style(edge_to_update),
-            )
-
-        edge.add_updater(shortest_path_updater)
-        edge.add_updater(shortest_path_updater)
-
+        edge = Edge(
+            vertex_one,
+            vertex_two,
+            **kwargs,
+        )
         self.add(edge)
 
-        self.adjacency_list[vertex_one].append(vertex_two)
+        self.adjacency_list[edge.vertex_one].append(edge.vertex_two)
 
-        self.vertices.add(vertex_one)
-        self.vertices.add(vertex_two)
+        self.vertices.add(edge.vertex_one)
+        self.vertices.add(edge.vertex_two)
 
     def get_vertex(self, label: str, /) -> Vertex:
         for vertex in self.vertices:
