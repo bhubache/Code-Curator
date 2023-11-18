eb4d6674340fdf3359bd3e07f1764ec3a967cd28
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-11-18 17:17:55 -0600

    Testing

--- src/code_curator/main.py
+++ src/code_curator/main.py
@@ -245,95 +245,186 @@ from manim import config
 
 class TestScene(Scene):
     config["background_color"] = "#FFFFFF"
+    config["disable_caching"] = True
 
     def construct(self):
-        graph = Graph()
-        graph.add_vertex(
-            "u",
-            contents="1/4",
-            label_out=True,
-            label_revolve_angle_in_degrees=90,
-        )
-        graph.add_vertex(
-            "v",
-            label_out=True,
-            position=(1.0, 0.0, 0.0),
-            label_revolve_angle_in_degrees=90,
-        )
-        graph.add_vertex(
-            "w",
-            label_out=True,
-            position=(2.0, 0.0, 0.0),
-            label_revolve_angle_in_degrees=90,
-        )
-        graph.add_vertex(
-            "x",
-            label_out=True,
-            position=(0.0, -1.0, 0.0),
-            label_revolve_angle_in_degrees=-90,
-        )
-        graph.add_vertex(
-            "y",
-            label_out=True,
-            position=(1.0, -1.0, 0.0),
-            label_revolve_angle_in_degrees=-90,
-        )
-        graph.add_vertex(
-            "z",
-            label_out=True,
-            position=(2.0, -1.0, 0.0),
-            label_revolve_angle_in_degrees=-90,
-        )
-
-        graph.add_edge(
-            "u",
-            "v",
-            directedness="->"
-        )
-        graph.add_edge(
-            "u",
-            "x",
-            directedness="->"
-        )
-        graph.add_edge(
-            "v",
-            "y",
-            directedness="->"
-        )
-        graph.add_edge(
-            "y",
-            "x",
-            directedness="->"
-        )
-        graph.add_edge(
-            "x",
-            "v",
-            directedness="->"
-        )
-        graph.add_edge(
-            "w",
-            "y",
-            directedness="->"
-        )
-        graph.add_edge(
-            "w",
-            "z",
-            directedness="->"
-        )
-        # graph.add_edge(
+        # graph = Graph()
+        # graph.add_vertex(
+        #     "u",
+        #     contents="1/4",
+        #     label_out=True,
+        #     label_revolve_angle_in_degrees=90,
+        # )
+        # graph.add_vertex(
+        #     "v",
+        #     label_out=True,
+        #     position=(1.0, 0.0, 0.0),
+        #     label_revolve_angle_in_degrees=90,
+        # )
+        # graph.add_vertex(
+        #     "w",
+        #     label_out=True,
+        #     position=(2.0, 0.0, 0.0),
+        #     label_revolve_angle_in_degrees=90,
+        # )
+        # graph.add_vertex(
+        #     "x",
+        #     label_out=True,
+        #     position=(0.0, -1.0, 0.0),
+        #     label_revolve_angle_in_degrees=-90,
+        # )
+        # graph.add_vertex(
+        #     "y",
+        #     label_out=True,
+        #     position=(1.0, -1.0, 0.0),
+        #     label_revolve_angle_in_degrees=-90,
+        # )
+        # graph.add_vertex(
         #     "z",
+        #     label_out=True,
+        #     position=(2.0, -1.0, 0.0),
+        #     label_revolve_angle_in_degrees=-90,
+        # )
+
+        # graph.add_edge(
+        #     "u",
+        #     "v",
+        #     directedness="->"
+        # )
+        # graph.add_edge(
+        #     "u",
+        #     "x",
+        #     directedness="->"
+        # )
+        # graph.add_edge(
+        #     "v",
+        #     "y",
+        #     directedness="->"
+        # )
+        # graph.add_edge(
+        #     "y",
+        #     "x",
+        #     directedness="->"
+        # )
+        # graph.add_edge(
+        #     "x",
+        #     "v",
+        #     directedness="->"
+        # )
+        # graph.add_edge(
+        #     "w",
+        #     "y",
+        #     directedness="->"
+        # )
+        # graph.add_edge(
+        #     "w",
         #     "z",
         #     directedness="->"
         # )
-        graph.move_to([0.0, 0.0, 0.0])
-        self.add(graph)
+        # # graph.add_edge(
+        # #     "z",
+        # #     "z",
+        # #     directedness="->"
+        # # )
+        # graph.move_to([0.0, 0.0, 0.0])
+        # self.add(graph)
 
-        self.play(
-            graph.get_vertex("z").animate.move_to([3, 1, 0]),
-            graph.get_vertex("u").animate.move_to([-3, 2, 0]),
-            graph.get_vertex("w").animate.move_to([0, -3, 0]),
-            run_time=5.0,
-        )
+        # graph.suspend_updating()
+        # self.play(graph.animate.move_to([-2, 2, 0]))
+        # self.play(
+        #     graph.get_vertex("x").animate.move_to([3, 1, 0]),
+        #     graph.get_vertex("u").animate.move_to([-3, 2, 0]),
+        #     graph.get_vertex("w").animate.move_to([0, -3, 0]),
+        #     run_time=1.0,
+        # )
+
+        # sll = Graph()
+        # sll.add_vertex(
+        #     contents=1,
+        #     show_label=False,
+        # )
+        # sll.add_vertex(
+        #     contents=2,
+        #     show_label=False,
+        #     position=(1.0, 0.0, 0.0),
+        # )
+        # sll.add_vertex(
+        #     contents=3,
+        #     show_label=False,
+        #     position=(2.0, 0.0, 0.0),
+        # )
+        # sll.add_vertex(
+        #     contents=4,
+        #     show_label=False,
+        #     position=(3.0, 0.0, 0.0),
+        # )
+        # sll.add_vertex(
+        #     contents="null",
+        #     show_label=False,
+        #     show_container=False,
+        #     position=(4.0, 0.0, 0.0),
+        # )
+        # sll.add_edge(
+        #     "Label0",
+        #     "Label1",
+        #     directedness="->",
+        # )
+        # sll.add_edge(
+        #     "Label1",
+        #     "Label2",
+        #     directedness="->"
+        # )
+        # sll.add_edge(
+        #     "Label2",
+        #     "Label3",
+        #     directedness="->",
+        # )
+        # sll.add_edge(
+        #     "Label3",
+        #     "Label4",
+        #     directedness="->",
+        # )
+        # self.add(sll)
+
+        # from manim import UP
+        # from manim import RIGHT
+        # from manim import LEFT, DOWN
+        # from code_curator.data_structures.graph import LabeledLine
+
+        # line = LabeledLine([0.0, -1.0, 0.0], sll.get_vertex("Label0"), label="trav")
+        # self.add(line)
+        # self.play(
+        #     sll.get_vertex("Label1").animate.shift(UP),
+        #     sll.get_vertex("Label3").animate.shift(LEFT + DOWN),
+        #     sll.get_vertex("Label0").animate.shift(LEFT),
+        #     # line.animate.move_to(sll.get_vertex("Label1"))
+        # )
+
+        from code_curator.data_structures.singly_linked_list_v2 import SinglyLinkedList
+        from code_curator.animations.singly_linked_list.transform_sll import TransformSinglyLinkedList
+        from manim import BLACK
+        from manim import MoveToTarget
+        from manim import Transform
+
+        sll = SinglyLinkedList(1, 2, 3, 4, 5, show_null=True, color=BLACK)
+        self.add(sll)
+
+        # self.wait(0.2)
+        # self.play(TransformSinglyLinkedList(sll, other))
+        # self.play(FadeTransformPieces(sll, SinglyLinkedList(1, 2, 3, 5, show_null=True, color=BLACK)), run_time=3.0)
+
+        sll.add_labeled_pointer(0, "pointer")
+        self.wait()
+
+        other = sll.copy()
+        other.remove_labeled_pointer("pointer")
+        other.add_labeled_pointer(1, "pointer")
+
+        self.play(TransformSinglyLinkedList(sll, other))
+
+
+
+
 
 
 if __name__ == "__main__":
