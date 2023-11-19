456576ed27728dea358964613d917ee2dc809714
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-11-19 12:22:51 -0600

    Add node removal animations

--- src/code_curator/animations/singly_linked_list/transform_sll.py
+++ src/code_curator/animations/singly_linked_list/transform_sll.py
@@ -7,16 +7,24 @@ from manim import AnimationGroup
 from manim import FadeIn
 from manim import FadeOut
 from manim import Mobject
+from manim import Scene
 from manim import Transform
+
+from code_curator.custom_logging.custom_logger import CustomLogger
+from code_curator.data_structures.graph import Graph
 
 if TYPE_CHECKING:
     from code_curator.data_structures.singly_linked_list_v2 import SinglyLinkedList
+
+logger = CustomLogger.getLogger(__name__)
 
 
 class TransformSinglyLinkedList(AnimationGroup):
     def __init__(self, mobject: SinglyLinkedList, target_mobject: SinglyLinkedList, **kwargs) -> None:
         self.length_change: int = len(target_mobject) - len(mobject)
+        self.kwargs = kwargs
 
+        self.mobject = mobject
         self.target_mobject = target_mobject
         self.submobjects_to_fade_out = []
         self.submobjects_to_fade_in = []
@@ -30,14 +38,14 @@ class TransformSinglyLinkedList(AnimationGroup):
             self.shorter_list = min(mobject, target_mobject, key=len)
             self.longer_list = max(mobject, target_mobject, key=len)
 
+        # TODO: Seprate handling of nodes and pointers
         short_node = next(iter(self.shorter_list))
         for long_node in self.longer_list:
             if short_node.value == long_node.value:
                 if self.length_change < 0:
                     self.matching_submobject_pairs.append((long_node, short_node))
                     self.matching_submobject_pairs.append((long_node.next_pointer, short_node.next_pointer))
-
-                if self.length_change > 0:
+                else:
                     self.matching_submobject_pairs.append((short_node, long_node))
                     self.matching_submobject_pairs.append((short_node.next_pointer, long_node.next_pointer))
 
@@ -58,13 +66,21 @@ class TransformSinglyLinkedList(AnimationGroup):
         original_labeled_line_labels = set(mobject.labeled_pointers.keys())
         target_labeled_line_labels = set(target_mobject.labeled_pointers.keys())
         self.submobjects_to_fade_out.extend(
-            list(original_labeled_line_labels - target_labeled_line_labels),
+            [
+                self.mobject.labeled_pointers[label]
+                for label in list(original_labeled_line_labels - target_labeled_line_labels)
+            ],
         )
         self.submobjects_to_fade_in.extend(
-            list(target_labeled_line_labels - original_labeled_line_labels),
+            [
+                self.target_mobject.labeled_pointers[label]
+                for label in list(target_labeled_line_labels - original_labeled_line_labels)
+            ],
         )
 
         for label in original_labeled_line_labels.intersection(target_labeled_line_labels):
+            mobject.labeled_pointers[label].suspend_updating()
+            target_mobject.labeled_pointers[label].suspend_updating()
             self.matching_submobject_pairs.append(
                 (mobject.labeled_pointers[label], target_mobject.labeled_pointers[label]),
             )
@@ -76,7 +92,40 @@ class TransformSinglyLinkedList(AnimationGroup):
         for submobject in self.submobjects_to_fade_in:
             animations.append(FadeIn(submobject))
 
+        # FIXME: There's a pair of None??? Perhaps one of the vertices of an edge wasn't set
         for submobject, target_submobject in self.matching_submobject_pairs:
             animations.append(Transform(submobject, target_submobject))
 
         super().__init__(*animations)
+
+    def clean_up_from_scene(self, scene: Scene) -> None:
+        super().clean_up_from_scene(scene)
+        try:
+            scene.replace(self.mobject, self.target_mobject)
+        except ValueError:
+            logger.warning(
+                "When a submobject is removed from a parent mobject, manim extracts all submobjects from parent and"
+                " places them in scene.mobjects",
+            )
+            mobjects = []
+            for mob in scene.mobjects:
+                if mob in self.mobject:
+                    if self.target_mobject not in mobjects:
+                        mobjects.append(self.target_mobject)
+
+                    continue
+
+                mobjects.append(mob)
+
+            scene.mobjects = mobjects
+
+        logger.warning(
+            "It seems an extra ``Graph`` is being added to the screen. Removing it. This may interfere with animations"
+            " that actually intend to have a graph as well",
+        )
+        mobjects_without_graph = []
+        for mobject in scene.mobjects:
+            if not isinstance(mobject, Graph):
+                mobjects_without_graph.append(mobject)
+
+        scene.mobjects = mobjects_without_graph
