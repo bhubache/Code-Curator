bd6caa6584317d55e80e9953cf3145a07725c242
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-12-06 18:57:26 -0600

    Remove unused code

--- src/code_curator/data_structures/pointers/null_pointer.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from code_curator.null_vmobject import NullVMobject
-
-from .base_pointer import BasePointer
-
-
-class NullPointer(NullVMobject, BasePointer):
-    """A null pointer"""
--- tests/test_non_graphical_units/test_data_structures/test_edges/test_weights/test_null_weight.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from src.data_structures.edges.weights.null_weight import NullWeight
-
-
-def test_value() -> None:
-    assert NullWeight().value is None
