03d8fa904167411f79d5b1df9919df28388dd197
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-12-10 12:37:21 -0600

    Abstract quasi label submobject on Vertex to
    CustomVMobject and clean class up

--- src/code_curator/custom_vmobject.py
+++ src/code_curator/custom_vmobject.py
@@ -1,128 +1,27 @@
 from __future__ import annotations
 
-from collections.abc import Sequence
+import itertools as it
+from typing import TYPE_CHECKING
 
-from code_curator.custom_logging.custom_logger import CustomLogger
+import numpy as np
 from manim import VMobject
 from manim.constants import DEFAULT_STROKE_WIDTH
+from manim.utils.family_ops import restructure_list_to_exclude_certain_family_members
+
+from code_curator.custom_logging.custom_logger import CustomLogger
 from code_curator.null_vmobject import NullVMobject
+
+if TYPE_CHECKING:
+    from manim.typing import Point3D_Array
+
+
 logger = CustomLogger.getLogger(__name__)
 
 
-def disregard_temp_submobjects(func):
-    def inner(self, *args, **kwargs):
-        # temp_mobj_map: dict = {}
-        if self._is_temp():
-            raise Exception('self is temp!')
-
-        mobj_copy = self.copy()
-
-        def disregard_temps(mobj):
-            for sub in mobj.submobjects:
-                try:
-                    if sub.is_temp():
-                        mobj.remove(sub)
-                    else:
-                        disregard_temps(sub)
-                except AttributeError:
-                    disregard_temps(sub)
-
-        disregard_temps(mobj_copy)
-        return func(mobj_copy, *args, **kwargs)
-    return inner
-
-
 class CustomVMobject(VMobject):
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-
-    # def __getattr__(self, name):
-    #     try:
-    #         value = super().__getattr__(name)
-    #     except AttributeError as e:
-    #         # print(e)
-    #         # value =
-    #         print(e)
-    #         raise AttributeError()
-    #     else:
-    #         return value
-
-    def _is_temp(self) -> bool:
-        return False
-
-    @disregard_temp_submobjects
-    def get_left(self) -> Sequence[float]:
-        return super().get_left()
-
-    @disregard_temp_submobjects
-    def get_right(self) -> Sequence[float]:
-        return super().get_right()
-
-    @disregard_temp_submobjects
-    def get_top(self) -> Sequence[float]:
-        return super().get_top()
-
-    @disregard_temp_submobjects
-    def get_bottom(self) -> Sequence[float]:
-        return super().get_bottom()
-
-    @disregard_temp_submobjects
-    def get_center(self) -> Sequence[float]:
-        return super().get_center()
-
-    def make_temp(self) -> CustomVMobject:
-        """
-        The presence of a submobject that is temporary
-        is not taken into consideration when getting
-        the relative locations of the parent mobject
-        or when moving the parent mobject.
-        """
-        self._is_temp = lambda: True
-        return self
-
-    # def move_to(
-    #     self,
-    #     point_or_mobject,
-    #     aligned_edge=ORIGIN,
-    #     coor_mask=np.array([1, 1, 1]),
-    # ):
-    #     """Move center of the :class:`~.Mobject` to certain coordinate."""
-    #     if isinstance(point_or_mobject, Mobject):
-    #         target = point_or_mobject.get_critical_point(aligned_edge)
-    #     else:
-    #         target = point_or_mobject
-
-    #     # mobj_copy = self.copy()
-    #     def disregard_temps(mobj):
-    #         for sub in mobj.submobjects:
-    #             try:
-    #                 if 'custom' == sub._label._value:
-    #                     print(sub._is_temp())
-    #             except Exception:
-    #                 pass
-    #             try:
-    #                 if sub._is_temp():
-    #                     print(f'removing {sub}')
-    #                     mobj.remove(sub)
-    #                 else:
-    #                     disregard_temps(sub)
-    #             except AttributeError:
-    #                 disregard_temps(sub)
-    #     disregard_temps(self)
-    #     point_to_align = self.get_critical_point(aligned_edge)
-    #     self.shift((target - point_to_align) * coor_mask)
-    #     return self
-
-    def add(self, *mobjects: VMobject) -> None:
-        non_null_vmobjects: list[VMobject] = []
-        for mob in mobjects:
-            if mob is None:
-                raise Exception(
-                    f'Can\'t add NoneType to submobjects: {mobjects}',
-                )
-            elif isinstance(mob, VMobject) and not isinstance(mob, NullVMobject):
-                non_null_vmobjects.append(mob)
-        super().add(*non_null_vmobjects)
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.quasi_mobjects: list[VMobject] = []
 
     @property
     def stroke_width(self) -> int:
@@ -131,3 +30,48 @@ class CustomVMobject(VMobject):
     @stroke_width.setter
     def stroke_width(self, _: int) -> None:
         pass
+
+    def add(self, *mobjects: VMobject) -> None:
+        non_null_vmobjects: list[VMobject] = []
+        for mob in mobjects:
+            if mob is None:
+                raise TypeError(
+                    f"Can't add NoneType to submobjects: {mobjects}",
+                )
+
+            if isinstance(mob, VMobject) and not isinstance(mob, NullVMobject):
+                non_null_vmobjects.append(mob)
+
+        super().add(*non_null_vmobjects)
+
+    def quasi_add(self, *mobjects: VMobject) -> None:
+        """Add each mobject in ``mobjects`` to ``self`` such that they don't contribute to boundary.
+
+        A mobject will be added to ``self.submobjects`` and will move with ``self``. It will not however,
+        contribute to the points that define the boundary of ``self``. This is seen with labels on vertices.
+        Regardless of where the label is relative to the rest of the vertex, the vertex will be positioned as
+        if the label is not part of the mobject, with the label maintaing the same relative distance. This can
+        be thought of as just adding an updater to the label such that it follows the vertex. That solution, as
+        far as I know, is too cumbersome to achieve as it would require making the scene an attribute of vertex,
+        which leads to issues when making copies of mobjects.
+
+        Args:
+            mobjects: The VMobjects to be added in a quasi manner
+        """
+        self.add(*mobjects)
+        self.quasi_mobjects.extend(mobjects)
+
+    def get_points_defining_boundary(self) -> Point3D_Array:
+        return np.array(
+            tuple(
+                it.chain(
+                    *(
+                        sm.get_anchors()
+                        for sm in restructure_list_to_exclude_certain_family_members(
+                            self.get_family(),
+                            to_remove=self.quasi_mobjects,
+                        )
+                    ),
+                ),
+            ),
+        )
