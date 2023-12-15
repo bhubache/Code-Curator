0b81ce0f43d79a82908b9add1866e3c096838296
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-12-15 10:20:10 -0600

    Begin animating sll with ``animate`` property

--- src/code_curator/base_scene.py
+++ src/code_curator/base_scene.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-
 from manim import config
 from manim import Mobject
 from manim import Scene
 
-from code_curator import constants
 from code_curator.animations.curator_animation_new import CuratorAnimation
 from code_curator.animations.curator_animation_new import ExcludeDuplicationSubmobjectsMobject
 from code_curator.custom_logging.custom_logger import CustomLogger
@@ -56,6 +54,10 @@ class BaseScene(Scene):
         super().__init__(**kwargs)
         self.animation_script = animation_script
 
+    @property
+    def scene_mobjects(self) -> list[Mobject]:
+        return self.mobjects[0].submobjects
+
     def construct(self) -> None:
         self.play(
             CuratorAnimation(
@@ -64,30 +66,3 @@ class BaseScene(Scene):
                 run_time=self.animation_script.run_time,
             ),
         )
-
-    @property
-    def scene_mobjects(self) -> list[Mobject]:
-        return self.mobjects[0].submobjects
-
-    def replace(self, old_mobject: Mobject, new_mobject: Mobject) -> None:
-        self.mobjects[0].submobjects.remove(old_mobject)
-        # TODO: May have to insert new_mobject at the same index old_mobject was at
-        self.mobjects[0].submobjects.append(new_mobject)
-
-        # Keep only the copies of data structures
-        copy_to_original = {}
-        for i, submobject_outer in enumerate(self.mobjects[0].submobjects):
-            for submobject_inner in self.mobjects[0].submobjects[i + 1 :]:
-                try:
-                    if submobject_inner.original_id == str(id(submobject_outer)):
-                        copy_to_original[submobject_inner] = submobject_outer
-                        break
-                except AttributeError:
-                    continue
-
-        for submobject in copy_to_original:
-            if submobject not in self.mobjects[0].submobjects:
-                self.mobjects[0].submobjects.append(submobject)
-
-        for submobject in copy_to_original.values():
-            self.mobjects[0].submobjects.remove(submobject)
