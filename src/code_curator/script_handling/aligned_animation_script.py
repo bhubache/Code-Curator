6b88b0707694dbb539cc1fdc4489fc591c100fbb
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-12-15 21:37:25 -0600

    More closely follow law of demeter

--- src/code_curator/script_handling/aligned_animation_script.py
+++ src/code_curator/script_handling/aligned_animation_script.py
@@ -1,16 +1,18 @@
 from __future__ import annotations
 
-import copy
-from collections.abc import Sequence
+from typing import TYPE_CHECKING
 
-from .components.alignment_script.alignments.aligned_script import AlignedScript
-from .components.animation_script.animation_leaf import AnimationLeaf
-from .components.animation_script.composite_animation_script import CompositeAnimationScript
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+    from .components.alignment_script.alignments.aligned_script import AlignedScript
+    from .components.animation_script.animation_leaf import AnimationLeaf
+    from .components.animation_script.composite_animation_script import CompositeAnimationScript
 
 
 class AlignedAnimationScript:
     def __init__(self, aligned_script: AlignedScript, animation_script: CompositeAnimationScript):
-        self._aligned_script:   AlignedScript = aligned_script
+        self._aligned_script: AlignedScript = aligned_script
         self._animation_script: CompositeAnimationScript = animation_script
         self.run_time: float = aligned_script.get_full_duration()
 
@@ -20,11 +22,16 @@ class AlignedAnimationScript:
             for entry in stream.entries:
                 if entry["name"] != "-wait-":
                     entries_without_waits.append(entry)
+
             stream.entries = entries_without_waits
 
-    def get_scenes(self) -> list[CompositeAnimationScript | AnimationLeaf]:
-        return self._animation_script.children
+    @property
+    def entries(self):
+        return self._animation_script["Video"].entries
 
     @property
     def stream_names(self) -> Sequence[str]:
         return self._animation_script.keys()
+
+    def get_scenes(self) -> list[CompositeAnimationScript | AnimationLeaf]:
+        return self._animation_script.children
