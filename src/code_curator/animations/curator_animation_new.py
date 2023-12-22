ce781d5c102f6618c2233af6670526a75a322a81
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-12-22 10:01:04 -0600

    Fix base scene singleton mobjects

--- src/code_curator/animations/curator_animation_new.py
+++ src/code_curator/animations/curator_animation_new.py
@@ -21,117 +21,16 @@ from .utils.math_ import value_from_range_to_range
 # TODO: DO prepare_animation before animation start?!
 
 
-class _MobjectSentinel(Mobject):
-    def __new__(cls):
-        if not hasattr(cls, "singleton_instance"):
-            cls.instance = super().__new__(cls)
-
-        return cls.instance
-
-
-class ExcludeDuplicationSubmobjectsMobject(Mobject):
-    def remove(self, *mobjects: Mobject) -> Mobject:
-        # If more than one mobject has been passed to a single FadeOut animation,
-        # all the mobjects will be wrapped in a Group. So, we need to iterate over
-        # the group to remove each mobject.
-        for mobject_to_remove in mobjects:
-            if isinstance(mobject_to_remove, Group):
-                for submobject_to_remove in mobject_to_remove:
-                    self.remove(submobject_to_remove)
-
-            self._remove(
-                mobject_to_remove=mobject_to_remove,
-                mobject_to_search=self,
-                mobject_container=None,
-            )
-
-        self._remove_all_sentinels()
-
-        return self
-
-    def _remove(
-        self,
-        *,
-        mobject_to_remove: Mobject,
-        mobject_to_search: Mobject,
-        mobject_container: Mobject | None,
-    ) -> None:
-        try:
-            problem_tex_parent = mobject_to_search.problem_tex_parent
-        except AttributeError:
-            pass
-        else:
-            if mobject_to_remove is problem_tex_parent:
-                self._place_sentinel(
-                    mobject_container=mobject_container,
-                    mobject_to_remove=mobject_to_search,
-                )
-
-        if mobject_to_remove in mobject_to_search.submobjects:
-            self._place_sentinel(
-                mobject_container=mobject_to_search,
-                mobject_to_remove=mobject_to_remove,
-            )
-
-        # FIXME: I think some mobjects aren't being removed because we're modifying
-        #   the length of mobject_to_search.submobjects while iterating?
-
-        for mobject in mobject_to_search.submobjects:
-            self._remove(
-                mobject_to_remove=mobject_to_remove,
-                mobject_to_search=mobject,
-                mobject_container=mobject_to_search,
-            )
-
-    def _place_sentinel(
-        self,
-        *,
-        mobject_container: Mobject,
-        mobject_to_remove: Mobject,
-    ) -> None:
-        index: int = mobject_container.submobjects.index(mobject_to_remove)
-        mobject_container.submobjects[index] = _MobjectSentinel()
-
-    def _remove_all_sentinels(self) -> None:
-        # TODO: Remove empty Groups?
-        new_submobjects = []
-        for submobject in self.submobjects:
-            if isinstance(submobject, _MobjectSentinel):
-                continue
-
-            try:
-                submobject.submobjects = self._remove_all_sentinels_helper(submobject)
-            except AttributeError:
-                pass
-            finally:
-                new_submobjects.append(submobject)
-
-        self.submobjects = new_submobjects
-
-    def _remove_all_sentinels_helper(self, mobject: Mobject):
-        new_submobjects: list[Mobject] = []
-        for submobject in mobject.submobjects:
-            if isinstance(submobject, _MobjectSentinel):
-                continue
-
-            try:
-                submobject.submobjects = self._remove_all_sentinels_helper(submobject)
-            except AttributeError:
-                pass
-            finally:
-                new_submobjects.append(submobject)
-
-        return new_submobjects
 
 
 class CuratorAnimation(Animation):
-    def __init__(self, animation_script, scene, run_time: float) -> None:
-        super().__init__(ExcludeDuplicationSubmobjectsMobject(), run_time=run_time, suspend_mobject_updating=True)
+    def __init__(self, mobject, animation_script, scene, run_time: float) -> None:
+        super().__init__(mobject, run_time=run_time, suspend_mobject_updating=True)
         self.animation_script = animation_script
         self.scene = scene
 
         # TODO: See if this is actually needed
-        setattr(self.scene, "mobjects", [self.mobject])
+        # setattr(self.scene, "mobjects", [self.mobject])
 
         self.pending_queue = collections.deque()
         for method_info in animation_script.entries:
