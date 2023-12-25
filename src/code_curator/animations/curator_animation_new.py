ad44194fdc4af05bc96d276917752eec8d4c0f44
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-12-24 23:55:59 -0600

    CUR-12: Remove unused code

--- src/code_curator/animations/curator_animation_new.py
+++ src/code_curator/animations/curator_animation_new.py
@@ -2,22 +2,12 @@ from __future__ import annotations
 
 import collections
 from collections.abc import Iterable
-from collections.abc import Sequence
 from typing import Callable
 
 from manim import Animation
-from manim import Mobject
 from manim import prepare_animation
 
 from .utils.math_ import value_from_range_to_range
-
-
-# NOTE: I believe a mobject must be added to Animation.mobject for it to be animated!!!
-
-# TODO: REFACTOR!
-# TODO: Overriding animations
-# TODO: Consider using a frame counter rather than mapping alpha to self.alphas element
-# TODO: DO prepare_animation before animation start?!
 
 
 class CuratorAnimation(Animation):
@@ -25,9 +15,6 @@ class CuratorAnimation(Animation):
         super().__init__(mobject, run_time=run_time)
         self.animation_script = animation_script
         self.scene = scene
-
-        # TODO: See if this is actually needed
-        # setattr(self.scene, "mobjects", [self.mobject])
 
         self.pending_queue = collections.deque()
         for method_info in animation_script.entries:
@@ -94,44 +81,4 @@ class AnimationPool:
             else:
                 anim.finish()
                 anim.clean_up_from_scene(self.scene)
-                # self.mobject.remove(
-                #     *self._get_remover_animation_mobjects(anim),
-                # )
-                # self.mobject.add(
-                #     *self._get_introducer_animation_mobjects(anim),
-                # )
                 self.animations.remove(anim)
-
-    def _get_remover_animation_mobjects(self, animation: Animation) -> Sequence[Mobject]:
-        mobjects_to_be_removed: list[Mobject] = []
-        if animation.is_remover():
-            mobjects_to_be_removed.append(animation.mobject)
-        else:
-            try:
-                child_animations = animation.animations
-            except AttributeError:
-                pass
-            else:
-                for child_anim in child_animations:
-                    mobjects_to_be_removed.extend(
-                        self._get_remover_animation_mobjects(child_anim),
-                    )
-
-        return mobjects_to_be_removed
-
-    def _get_introducer_animation_mobjects(self, animation: Animation) -> Sequence[Mobject]:
-        mobjects_to_be_introduced: list[Mobject] = []
-        if animation.is_introducer():
-            mobjects_to_be_introduced.append(animation.mobject)
-        else:
-            try:
-                child_animations = animation.animations
-            except AttributeError:
-                pass
-            else:
-                for child_anim in child_animations:
-                    mobjects_to_be_introduced.extend(
-                        self._get_introducer_animation_mobjects(child_anim),
-                    )
-
-        return mobjects_to_be_introduced
