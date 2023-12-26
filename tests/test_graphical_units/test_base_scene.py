3622bedf895d48ad206e0412c31739849be34f4e
Brandon Hubacher <brandon.hubacher@gmail.com>
2023-12-26 09:50:07 -0600

    CUR-14: Add some more tests

--- tests/test_graphical_units/test_base_scene.py
+++ tests/test_graphical_units/test_base_scene.py
@@ -63,6 +63,40 @@ class test_removing_mobject_from_scene_makes_it_disappear_from_screen:
 
 
 @curator_frames_comparison
+class test_removing_multiple_mobjects_from_scene_makes_them_disappear_from_screen:
+    def __init__(self, scene: BaseScene) -> None:
+        self.scene = scene
+
+    def method(self):
+        square = Square()
+        circle = Circle().move_to((2, 2, 0))
+        arrow = Arrow().move_to((-2, -2, 0))
+        self.scene.add(square)
+        self.scene.add(circle)
+        self.scene.add(arrow)
+
+        self.scene.remove(square)
+        self.scene.remove(circle)
+        self.scene.remove(arrow)
+
+
+@curator_frames_comparison
+class test_removing_one_mobject_with_multiple_on_screen_makes_the_correct_one_disappear_from_screen:
+    def __init__(self, scene: BaseScene) -> None:
+        self.scene = scene
+
+    def method(self):
+        square = Square()
+        circle = Circle().move_to((2, 2, 0))
+        arrow = Arrow().move_to((-2, -2, 0))
+        self.scene.add(square)
+        self.scene.add(circle)
+        self.scene.add(arrow)
+
+        self.scene.remove(square)
+
+
+@curator_frames_comparison
 class test_clearing_scene_makes_all_mobjects_disappear_from_screen:
     def __init__(self, scene: BaseScene) -> None:
         self.scene = scene
@@ -119,18 +153,6 @@ class test_removing_multiple_mobjects_at_the_same_time:
         return FadeOut(square), FadeOut(circle)
 
 
-@curator_frames_comparison(last_frame=False)
-class test_fade_in_and_fade_out_mobjects_at_the_same_time:
-    def __init__(self, scene: BaseScene) -> None:
-        self.scene = scene
-
-    def animation(self):
-        square = Square().move_to((1, 1, 0))
-        self.scene.add(square)
-
-        return FadeIn(Circle()), FadeOut(square)
-
-
 @curator_frames_comparison(last_frame=False, run_time=1.5)
 class test_overlapping_fade_in_animations_of_different_mobjects:
     def __init__(self, scene: BaseScene) -> None:
@@ -148,14 +170,14 @@ class test_overlapping_fade_in_animations_of_different_mobjects:
 class test_overlapping_fade_out_animations_of_different_mobjects:
     def __init__(self, scene: BaseScene) -> None:
         self.scene = scene
+        self.circle = Circle()
 
     def first_fade_in(self):
-        self.square = Square().move_to((1, 1, 0))
-        self.circle = Circle()
-        self.scene.add(self.square)
+        square = Square().move_to((1, 1, 0))
+        self.scene.add(square)
         self.scene.add(self.circle)
 
-        return FadeOut(self.square)
+        return FadeOut(square)
 
     @starts_at(0.5)
     def second_fade_in(self):
@@ -208,9 +230,9 @@ class test_multiple_animations_on_one_mobject_at_the_same_time:
 class test_overlapping_animations_on_one_mobject:
     def __init__(self, scene: BaseScene) -> None:
         self.scene = scene
+        self.circle = Circle()
 
     def first_animation(self):
-        self.circle = Circle()
         self.circle.set_opacity(0)
         self.scene.add(self.circle)
 
@@ -225,11 +247,11 @@ class test_overlapping_animations_on_one_mobject:
 class test_animations_that_start_at_the_same_time_but_have_different_run_times:
     def __init__(self, scene: BaseScene) -> None:
         self.scene = scene
+        self.second_fast_square = Square()
 
     def first_animation(self):
         slow_square = Square().move_to((1, 1, 0))
         first_fast_square = Square().move_to((-1, 1, 0))
-        self.second_fast_square = Square()
 
         self.scene.add(slow_square)
         self.scene.add(first_fast_square)
