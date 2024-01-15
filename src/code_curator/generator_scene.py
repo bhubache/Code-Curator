95ccbe1f816dd99ee7c29e1a35da32060a44977c
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-01-15 12:26:34 -0600

    CUR-16: Remove generator scene

--- src/code_curator/generator_scene.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from manim import Scene
-
-from .animations.animation_generator import AnimationGenerator
-
-
-class GeneratorScene(AnimationGenerator, Scene):
-    """Provide connection between custom code and manim scene."""
