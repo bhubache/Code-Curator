fddb48f9b1c73285bec16e8c532e2d2d92cf77c3
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-01-17 00:02:20 -0600

    CUR-18: Adjust start timings based on start marker

--- src/code_curator/script_handling/aligned_animation_script.py
+++ src/code_curator/script_handling/aligned_animation_script.py
@@ -14,7 +14,6 @@ class AlignedAnimationScript:
     def __init__(self, aligned_script: AlignedScript, animation_script: CompositeAnimationScript):
         self._aligned_script: AlignedScript = aligned_script
         self._animation_script: CompositeAnimationScript = animation_script
-        self.run_time: float = aligned_script.get_full_duration()
 
         for stream in animation_script.values():
             stream.apply_alignments(-1, -1, self._aligned_script)
@@ -24,6 +23,15 @@ class AlignedAnimationScript:
                     entries_without_waits.append(entry)
 
             stream.entries = entries_without_waits
+
+        start_time = 0.0
+        for entry in self.entries:
+            if entry.get("start_here", False):
+                start_time = entry["start_time"]
+                self.run_time = self._aligned_script.get_full_duration() - start_time
+
+        for index, _ in enumerate(self.entries):
+            self.entries[index]["start_time"] -= start_time
 
     @property
     def entries(self):
