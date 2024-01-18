2c3c2b7d8c884af937edccfc7622504b7f6fd1fa
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-01-17 20:56:46 -0600

    CUR-19: Fix move_to_line and make highlighter look
    pretty

--- src/code_curator/code/code_highlighter.py
+++ src/code_curator/code/code_highlighter.py
@@ -20,27 +20,32 @@ class CodeHighlighter(Rectangle):
         height: float | None = None,
         width: float | None = None,
         color: str | Color = YELLOW,
-        opacity: float = 0.5,
+        opacity: float = 0.2,
         stroke_width: float = 0.0,
         start_line: int = 1,
+        height_buff: float = 0.05,
+        width_buff: float = 0.1,
     ) -> None:
         if height is None:
-            height = code.max_line_height
+            height = code.max_line_height + height_buff
 
         if width is None:
-            width = code.max_line_width
+            width = code.max_line_width + width_buff
 
         super().__init__(height=height, width=width, color=color, stroke_width=stroke_width)
 
         self.code = code
+        self.height_buff = height_buff
+        self.width_buff = width_buff
         self.curr_line_num = start_line
         self.set_opacity(opacity)
-        self.move_to(
-            remove_invisible_chars(
-                self.code.get_line(self.curr_line_num).copy(),
-            ).get_center(),
-        )
-        self.align_to(self.code.code_mobject, LEFT)
+        self.move_to_line(start_line)
+
+    def reset_height(self):
+        return self.stretch_to_fit_height(self.code.max_line_height + self.height_buff)
+
+    def reset_width(self):
+        return self.stretch_to_fit_width(self.code.max_line_width + self.width_buff)
 
     def move_to_substring(self, substring: str, occurrence: int, num_lines: int | None):
         """
@@ -66,12 +71,25 @@ class CodeHighlighter(Rectangle):
 
         return self.animate.become(new_highlighter)
 
-    def move_to_line(self, line_num: int) -> None:
+    def move_to_line(self, line_num: int):
         if line_num < 1 or line_num > self.code.num_lines:
             raise ValueError(
                 f"``line_num`` must be in range [1, {self.code.num_lines}]. Given {line_num}",
             )
 
+        self.reset_height()
+        self.reset_width()
+
+        to_line_copy = self.code.get_line(line_num).copy()
+        try:
+            to_line_copy_with_only_visible_chars = remove_invisible_chars(to_line_copy)
+        except IndexError:
+            to_line_copy_with_only_visible_chars = self.code.get_line(line_num).copy()
+
+        self.move_to(to_line_copy_with_only_visible_chars.get_center())
+        self.align_to(self.code.code_mobject, LEFT)
+        self.shift(LEFT * (self.width_buff / 2))
+
         self.curr_line_num = line_num
 
-        return self.animate.become(CodeHighlighter(self.code).align_to(self.code.get_line(self.curr_line_num), DOWN))
+        return self
