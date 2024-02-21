8ff2ba208976b0f2410954706a910541819b201e
Brandon Hubacher <brandon.hubacher@gmail.com>
2024-02-21 14:49:28 -0600

    CUR-23: Clean use of forced aligner

--- src/code_curator/alignment_text_creation/_montreal_forced_aligner.py
+++ src/code_curator/alignment_text_creation/_montreal_forced_aligner.py
@@ -1,53 +1,53 @@
 from __future__ import annotations
 
 import logging
-import shlex
 import shutil
-import subprocess
-import tarfile
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import docker
+
+if TYPE_CHECKING:
+    import os
 
 logger = logging.getLogger(__name__)
 
 
-ALIGNMENT_HAS_CHANGED = False
+def perform_alignment(script_path: str | os.PathLike, audio_path: str | os.PathLike) -> Path:
+    script_path = Path(script_path)
+    audio_path = Path(audio_path)
 
+    volume_dir = Path("/", "tmp")
+    base_mfa_dir = volume_dir / "mfa"
+    input_mfa_dir = base_mfa_dir / "input"
+    output_mfa_dir = base_mfa_dir / "output"
 
-class MontrealForcedAligner:
+    shutil.rmtree(base_mfa_dir, ignore_errors=True)
+    input_mfa_dir.mkdir(parents=True)
+    output_mfa_dir.mkdir(parents=True)
 
-    @classmethod
-    def perform_alignment(cls, script_path: str | os.PathLike, audio_path: str | os.PathLike) -> Path:
-        volume_dir = Path("/", "tmp")
-        base_mfa_dir = volume_dir / "mfa"
-        input_mfa_dir = base_mfa_dir / "input"
-        output_mfa_dir = base_mfa_dir / "output"
+    shutil.copy(script_path, input_mfa_dir / "script.txt")
+    shutil.copy(audio_path, input_mfa_dir / "script.wav")
 
-        shutil.rmtree(base_mfa_dir, ignore_errors=True)
-        input_mfa_dir.mkdir(parents=True)
-        output_mfa_dir.mkdir(parents=True)
+    client = docker.from_env()
+    image_name = "mmcauliffe/montreal-forced-aligner"
+    image_tag = "latest"
 
-        shutil.copy(script_path, input_mfa_dir / "script.txt")
-        shutil.copy(audio_path, input_mfa_dir / "script.wav")
+    client.images.pull(image_name, tag=image_tag)
+    mfa_image = client.images.get(f"{image_name}:{image_tag}")
 
-        client = docker.from_env()
-        image_name = "mmcauliffe/montreal-forced-aligner"
-        image_tag = "latest"
+    client.containers.run(
+        mfa_image,
+        command=[
+            "bash",
+            "-c",
+            (
+                "mfa model download acoustic english_us_arpa && mfa model download dictionary english_us_arpa &&"
+                f" mfa align {input_mfa_dir} english_us_arpa english_us_arpa {output_mfa_dir}"
+            ),
+        ],
+        volumes=[f"{volume_dir}:{volume_dir}"],
+        tty=True,
+    )
 
-        client.images.pull(image_name, tag=image_tag)
-        mfa_image = client.images.get(f"{image_name}:{image_tag}")
-
-        client.containers.run(
-            mfa_image,
-            command=[
-                "bash",
-                "-c",
-                f"mfa model download acoustic english_us_arpa && mfa model download dictionary english_us_arpa && mfa align /tmp/mfa/input english_us_arpa english_us_arpa /tmp/mfa/output",
-            ],
-            volumes=[f"{volume_dir}:{volume_dir}"],
-            tty=True,
-        )
-
-        return output_mfa_dir / "script.TextGrid"
-
+    return output_mfa_dir / "script.TextGrid"
